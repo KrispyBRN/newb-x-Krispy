422d139f6c46ae4078dc1aaae7c277db7e1f5505
Coolguy <mobilemc022@gmail.com>
2026-09-10 22:18:09 +0700

    Update sky.h

--- src/newb/functions/sky.h
+++ src/newb/functions/sky.h
@@ -254,4 +254,6 @@ vec4 renderAuroraComplementary(vec3 viewDir, vec2 worldXZ, float time, float day
     return vec4(finalColor, auroraMask);
 }
 
-#endif // SKY_H
+   #endif // NL_AURORA
+
+   #endif // SKY_H
